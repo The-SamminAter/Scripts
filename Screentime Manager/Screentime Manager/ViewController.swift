4d03ed23b677efefe88366efccbf8add6de7363a
The-SamminAter <48739810+The-SamminAter@users.noreply.github.com>
2020-03-13 12:25:45 -0700

    I took an hour. To make a button with a background.

---
+++ Screentime Manager/Screentime Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Screentime Manager
+//
+//  Created by Student on 3/13/20.
+//  Copyright © 2020 The-SamminAter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
