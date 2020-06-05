abc401af2c4ecd0712be6959c2bb1d7bd7c40cfd
Sam <48739810+The-SamminAter@users.noreply.github.com>
2020-06-05 12:13:55 -0700

    Update sums.sh

--- sums.sh
+++ sums.sh
@@ -3,15 +3,15 @@
 #I made this for my modification of unc0ver, but it has other uses, like for my repo
 #Method for getting only the sums from here: http://stackoverflow.com/questions/1405611/ddg#1405641
 #if [ -z "$file" ]; then
-#  read -e -r -p "Please drag and drop the file here, and then press enter: " DEBDIR
+  read -e -r -p "Please drag and drop the file here, and then press enter: " DEBDIR
 #else
 #  DEBDIR="$file"
 #fi
-MD5L=$(md5sum "$file")
+MD5L=$(md5sum "${DEBDIR}")
 MD5S="${MD5LONG:0:32}"
-SHA1L=$(sha1sum "$file")
+SHA1L=$(sha1sum "${DEBDIR}")
 SHA1S="${SHA1LONG:0:40}"
-SHA256L=$(sha256sum "$file")
+SHA256L=$(sha256sum "${DEBDIR}")
 SHA256S="${SHA256LONG:0:64}"
 echo "MD5sum: ${MD5S}"
 echo "SHA1: ${SHA1S}"
