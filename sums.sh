c4f6df856cf41dbd3af533ec7c705074b4cc36d8
Sam <48739810+The-SamminAter@users.noreply.github.com>
2020-06-05 11:58:20 -0700

    Create sums.sh

---
+++ sums.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#This gives you copy-pasteable MD5, SHA1, and SHA256 values
+#I made this for my modification of unc0ver, but it has other uses, like for my repo
+#Method for getting only the sums from here: http://stackoverflow.com/questions/1405611/ddg#1405641
+if [ -z "$file" ];
+  read -e -p "Please drag and drop the file here, and then press enter: " DEBDIR;
+else
+  DEBDIR="$file"
+fi
+MD5L=$(md5sum "${DEBDIR}")
+MD5S="${MD5LONG:0:32}"
+SHA1L=$(sha1sum "${DEBDIR}")
+SHA1S="${SHA1LONG:0:40}"
+SHA256L=$(sha256sum "${DEBDIR}")
+SHA256S="${SHA256LONG:0:64}"
+echo "MD5sum: ${MD5S}"
+echo "SHA1: ${SHA1S}"
+echo "SHA256: ${SHA256S}"
