c14584c9d8e9ed2c4fa83510329169857d944cc4
Sam <48739810+The-SamminAter@users.noreply.github.com>
2021-03-09 14:55:41 -0800

    Extracts debs with The Unarchiver
    Yeah, I'm aware this could be done much better

---
+++ debextract.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Part 1:
+"/Applications/The Unarchiver.app/Contents/MacOS/The Unarchiver" "${1}"
+FP=$(echo "${1%%.deb}")
+rm "${FP}/debian-binary"
+cFile=$(ls "${FP}" | grep "control.tar")
+dFile=$(ls "${FP}" | grep "data.tar")
+"/Applications/The Unarchiver.app/Contents/MacOS/The Unarchiver" "${FP}/${cFile}"
+"/Applications/The Unarchiver.app/Contents/MacOS/The Unarchiver" "${FP}/${dFile}"
+rm "${FP}/${cFile}" "${FP}/${dFile}"
+mv "${FP}/control" "${FP}/DEBIAN"
+mv "${FP}/data/"* "${FP}/"
+rm -rf "${FP}/data"
